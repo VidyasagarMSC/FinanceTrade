b5e3b1e3345f0a9548da01bd97c4cdca14f165f2
Vidyasagar Machupalli <mscvidyasagar@gmail.com>
2017-09-12 15:42:04 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,3 @@ cf create-service discovery lite discovery
 cf create-service fss-portfolio-service fss-portfolio-service-free-plan investment-portfolio
 cf create-service fss-predictive-scenario-analytics-service fss-predictive-scenario-analytics-service-free-plan predictive-scenario-analytics
 cf create-service fss-scenario-analytics-service fss-scenario-analytics-service-free-plan scenario-analytics
-cf create-service appid bronze appid
